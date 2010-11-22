31d6b812080db5a9142e87fef4aa1898ca6f2711
Ildus Kurbangaliev <i.kurbangaliev@gmail.com>
2010-11-22 18:37:21 +0500

    fix

--- ipgeobase/models.py
+++ ipgeobase/models.py
@@ -1,7 +1,7 @@
 #coding: utf-8
 
 from django.db import models
-from django_ipgeobase.managers import IPGeoBaseManager
+from ipgeobase.managers import IPGeoBaseManager
 from django.utils.translation import ugettext_lazy as _
 
 class Country(models.Model):
